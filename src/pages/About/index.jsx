b45e280552a1a6ae66466950a5c818759b3ef6ff
Rafael Tivane <luislangabusiness@gmail.com>
2022-02-01 21:34:07 +0200

    working on responsiveness of hero section

--- src/pages/About/index.jsx
+++ src/pages/About/index.jsx
@@ -9,39 +9,43 @@ const About = () => {
     const globalStyles = useGlobalStyles();
 
     const ContentCard = useCallback(({ description, title }) => (
-        <div className={classNames('mb-8')}>
+        <div className={classNames('mb-8 sm:flex sm:items-start sm:justify-between')}>
             <Typography 
-                className={classNames('font-bold capitalize text-center', globalStyles.sanJuanBlueColor)}
-                component="h1"
+                className={classNames('font-bold capitalize text-center sm:text-left', globalStyles.sanJuanBlueColor)}
+                component="h2"
                 variant="h5">
                     { title }
             </Typography>
             <Typography 
-                className={classNames('text-center text-base mt-4', globalStyles.lightJuanBlueColor)}
+                className={classNames('text-center sm:text-left text-base mt-4 sm:mt-0 grow', 
+                globalStyles.lightJuanBlueColor, classes.contentCardDescription)}
                 component="p"
                 variant="body2">
                     { description }
             </Typography>
         </div>
-    ), [ globalStyles ])
+    ), [ classes, globalStyles ])
 
     return (
         <main>
             <section className={classNames('bg-no-repeat', globalStyles.hero, globalStyles.px)}>
-                <Typography 
-                    className={classNames('font-bold text-center mb-16', globalStyles.sanJuanBlueColor)}
-                    component="h1"
-                    variant="h4">
-                        We empower innovators by delivering access to the financial system
-                </Typography>
-                <ContentCard 
-                    description="Our main goal is to build beautiful consumer experiences along with developer-friendly infrastructure. The result is an intelligent tool that gives everyone the ability to create amazing products that solve big problems. We are deeply focused on democratizing financial services through technology. "
-                    title="Our vision"
-                />
-                <ContentCard 
-                    description="At the core of our platform is the technical infrastructure APIs that connect consumers. Our innovative product provides key insights for businesses and individuals, as well as robust reporting for traditional financial institutions and developers. "
-                    title="Our business"
-                />
+                <div className={classNames(classes.heroContent)}>
+                    <Typography 
+                        className={classNames('font-bold text-center md:text-left mb-16', 
+                        globalStyles.sanJuanBlueColor, classes.heroContentTitle)}
+                        component="h1"
+                        variant="h4">
+                            We empower innovators by delivering access to the financial system
+                    </Typography>
+                    <ContentCard 
+                        description="Our main goal is to build beautiful consumer experiences along with developer-friendly infrastructure. The result is an intelligent tool that gives everyone the ability to create amazing products that solve big problems. We are deeply focused on democratizing financial services through technology. "
+                        title="Our vision"
+                    />
+                    <ContentCard 
+                        description="At the core of our platform is the technical infrastructure APIs that connect consumers. Our innovative product provides key insights for businesses and individuals, as well as robust reporting for traditional financial institutions and developers. "
+                        title="Our business"
+                    />
+                </div>
             </section>
         </main>
     );
--- src/pages/About/styles.js
+++ src/pages/About/styles.js
@@ -1,3 +1,19 @@
 import { makeStyles } from '@mui/styles'
 
-export const useStyles = makeStyles(theme => ({}));
+export const useStyles = makeStyles(theme => ({
+    heroContent: {
+        [theme.breakpoints.up('md')]: {
+            padding: '0 5%'
+        }
+    },
+    heroContentTitle: {
+        [theme.breakpoints.up('md')]: {
+            maxWidth: '50%'
+        }
+    },
+    contentCardDescription: {
+        [theme.breakpoints.up('sm')]: {
+            maxWidth: '70%'
+        }
+    }
+}));
