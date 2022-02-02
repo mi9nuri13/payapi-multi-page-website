eafe224f2595c7728f22876def7d9e840d456b10
Rafael Tivane <luislangabusiness@gmail.com>
2022-02-02 20:24:24 +0200

    fixing footer background pattern responsiveness

--- src/components/Footer/styles.js
+++ src/components/Footer/styles.js
@@ -5,10 +5,22 @@ export const useStyles = makeStyles(theme => ({//
     footer: {
         backgroundColor: '#1B262F',
         backgroundImage: `url(${circlePattern})`,
-        backgroundPosition: 'center bottom -236px',
+        backgroundPosition: 'center bottom -227px',
+        backgroundSize: 400,
+        [theme.breakpoints.up(420)]: {
+            backgroundPosition: 'center bottom -324px',
+            backgroundSize: 500,
+        },
+        [theme.breakpoints.up(490)]: {
+            backgroundPosition: 'center bottom -524px',
+            backgroundSize: 700,
+        },
+        [theme.breakpoints.up('sm')]: {
+            backgroundPosition: 'right -183px bottom -217px',
+            backgroundSize: 400,
+        },
         [theme.breakpoints.up(1200)]: {
-            backgroundPosition: 'right -50px bottom -165px',
-            backgroundSize: '300px !important'
+            backgroundPosition: 'right -134px bottom -240px',
         }
     }
 }))
